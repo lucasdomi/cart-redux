5e3a771aeb86d6e22a9dc07f8a70f873db5cd768
Lucas Domingues <lucasdomi@globo.com>
2019-08-22 00:06:25 -0300

    Estilizando home da aplicação

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,106 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/500x500/91588631/tenis-adidas-ultraboost-masculino-img.jpg"
+          alt=""
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/500x500/91588631/tenis-adidas-ultraboost-masculino-img.jpg"
+          alt=""
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/500x500/91588631/tenis-adidas-ultraboost-masculino-img.jpg"
+          alt=""
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/500x500/91588631/tenis-adidas-ultraboost-masculino-img.jpg"
+          alt=""
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/500x500/91588631/tenis-adidas-ultraboost-masculino-img.jpg"
+          alt=""
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://imgcentauro-a.akamaihd.net/500x500/91588631/tenis-adidas-ultraboost-masculino-img.jpg"
+          alt=""
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
