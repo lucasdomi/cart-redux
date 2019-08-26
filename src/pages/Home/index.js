5b882ab07f09743dccde4b0f8fe63bc306fdadf8
Lucas Domingues <lucasdomi@globo.com>
2019-08-26 00:49:28 -0300

    Adicionando produto no carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { connect } from 'react-redux';
 import { ProductList } from './styles';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
-export default class Home extends Component {
+
+class Home extends Component {
   state = {
     products: [],
   };
@@ -16,6 +18,15 @@ export default class Home extends Component {
     }));
     this.setState({ products: data });
   }
+
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
   render() {
     const { products } = this.state;
     return (
@@ -26,7 +37,10 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted} </span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" /> 3
               </div>
@@ -39,3 +53,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
