93c1fd38a4733743bb58575524154455c985e7fd
Lucas Domingues <lucasdomi@globo.com>
2019-08-26 01:04:22 -0300

    Incluindo suporte ao reactotron

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
 import { createStore } from 'redux';
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+const enhancer =
+  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+
+const store = createStore(
+  rootReducer,
+  enhancer
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
